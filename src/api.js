a1cf3fdb0b3f88ab7ff33dfd8660c901e6fb94ed
bobbidigi <bobbidigi34@gmail.com>
2019-06-26 15:24:41 -0400

    add dummy array in api

---
+++ src/api.js
@@ -0,0 +1,2 @@
+let imageArr = ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300", ];
+
